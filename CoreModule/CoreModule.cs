7e943634c318b2d5b063e352c6bf3a61fcdbf782
Gavin White <horace5413@gmail.com>
2020-03-28 21:08:46 -0700

    Initial commit

---
+++ CoreModule/CoreModule.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreModule
+{
+    public static class Main
+    {
+        public static void Start() {
+            Console.WriteLine("Starting core engine...");
+        }
+    }
+}
