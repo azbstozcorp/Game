53753646730816a5b9ccd5f61fc355b609a7ad65
Gavin White <horace5413@gmail.com>
2020-03-29 10:47:56 -0700

    Basic shapes and intersection

---
+++ CoreModule/Collision.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreModule {
+    public static class Collision {
+        /// <summary>
+        /// Check a given value between two bounds
+        /// </summary>
+        public static bool Between(int lowerBound, int upperBound, int value, bool inclusive = false) =>
+             (lowerBound < value && upperBound < value) ||                  /* Check the value between upper and lower bounds    */
+             (inclusive && (value == lowerBound || value == upperBound));   /* If the check is inclusive, return true for bounds */
+
+        /// <summary>
+        /// Check if a given point is within a given rectangle
+        /// </summary>
+        public static bool WithinRect(Point topLeft, Point bottomRight, Point check, bool inclusive = false) =>
+            Between(topLeft.X, bottomRight.X, check.X, inclusive) &&   /* Check X coordinate */
+            Between(topLeft.Y, bottomRight.Y, check.Y, inclusive);     /* Check Y coordinate */
+
+        /// <summary>
+        /// Check if two rectangles overlap
+        /// </summary>
+        public static bool RectsOverlap(Rect a, Rect b) =>
+            a.Left < b.Right && a.Right > b.Left &&
+            a.Top < b.Bottom && a.Bottom > b.Top;
+
+        static bool CCW(Point a, Point b, Point c) => (c.Y - a.Y) * (b.X - a.X) > (b.Y - a.Y) * (c.X - a.X);
+        /// <summary>
+        /// Check if two lines intersect
+        /// </summary>
+        public static bool LinesIntersect(Line a, Line b) =>
+            CCW(a.Start, b.Start, b.End) != CCW(a.End, b.Start, b.End) && CCW(a.Start, a.End, b.Start) != CCW(a.Start, a.End, b.End);
+
+        /// <summary>
+        /// Find the intersection point of two lines
+        /// </summary>
+        public static Point IntersectionOf(Line a, Line b) {
+            int a1 = a.End.Y - a.Start.Y;
+            int b1 = a.Start.X - a.End.X;
+            int c1 = a1 * a.Start.X + b1 * a.Start.Y;
+
+            int a2 = b.End.Y - b.Start.Y;
+            int b2 = b.Start.X - b.End.X;
+            int c2 = a1 * b.Start.X + b1 * b.Start.Y;
+
+            int delta = a1 * b2 - a2 * b1;
+            return delta == 0 ? null : new Point((b2 * c1 - b1 * c2) / delta, (a1 * c2 - a2 * c1) / delta);
+        }
+
+        static int Min(int a, int b) => a > b ? b : a;
+        static int Max(int a, int b) => a > b ? a : b;
+    }
+}
---
+++ CoreModule/Shapes.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreModule {
+    /// <summary>
+    /// Point in 2d space
+    /// </summary>
+    [Serializable]
+    public class Point {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point() { }
+        public Point(int x, int y) { X = x; Y = y; }
+        public Point(PixelEngine.Point from) { X = from.X; Y = from.Y; }
+        public Point((int x, int y) from) { X = from.x; Y = from.y; }
+
+        public static bool operator ==(Point a, Point b) => a.Equals(b);
+        public static bool operator !=(Point a, Point b) => !(a == b);
+
+        public static implicit operator Point(PixelEngine.Point p) => new Point(p);
+        public static implicit operator Point((int x, int y) p) => new Point(p);
+        public static implicit operator PixelEngine.Point(Point p) => new PixelEngine.Point(p.X, p.Y);
+        public static implicit operator (int x, int y) (Point p) => (p.X, p.Y);
+
+        public override string ToString() => $"({X},{Y})";
+
+        public override bool Equals(object obj) {
+            Point other = obj as Point;
+            if (other != null)   /* If the object is a point, check for equality ---------- */
+                return other.X == X && other.Y == Y;
+            return false;        /* If the object isn't a point, there's no way it is equal */
+        }
+    }
+
+    [Serializable]
+    public class Line {
+        public Point Start {
+            get => a; set {
+                a = value;
+            }
+        }
+        public Point End {
+            get => b; set {
+                b = value;
+            }
+        }
+        Point a, b;
+
+        public Line() {
+            a = b = new Point();
+        }
+        public Line(Point a, Point b) : this() { Start = a; End = b; }
+
+        public static bool operator ==(Line a, Line b) => a.Start == b.Start && a.End == b.End;
+        public static bool operator !=(Line a, Line b) => !(a == b);
+    }
+
+    /// <summary>
+    /// Rectangle in 2d space
+    /// </summary>
+    [Serializable]
+    public class Rect {
+        public Point TopLeft {
+            get => topLeft; set {
+                topLeft = value;
+                topRight.Y = value.Y;
+                bottomRight.X = value.X;
+                MaintainStructure();
+            }
+        }
+        public Point TopRight {
+            get => topRight; set {
+                topRight = value;
+                topLeft.Y = value.Y;
+                bottomRight.X = value.X;
+                MaintainStructure();
+            }
+        }
+        public Point BottomLeft {
+            get => bottomLeft; set {
+                bottomLeft = value;
+                bottomRight.Y = value.Y;
+                topLeft.X = value.X;
+                MaintainStructure();
+            }
+        }
+        public Point BottomRight {
+            get => bottomRight; set {
+                bottomRight = value;
+                bottomLeft.Y = value.Y;
+                topRight.X = value.X;
+                MaintainStructure();
+            }
+        }
+
+        public int Top => TopLeft.Y;
+        public int Left => TopLeft.X;
+        public int Bottom => BottomRight.Y;
+        public int Right => BottomRight.X;
+
+        public Point topLeft, topRight, bottomLeft, bottomRight;
+
+        public Rect() => topLeft = topRight = bottomLeft = bottomRight = new Point();
+        public Rect(Point topLeft, Point bottomRight) : this() {
+            TopLeft = topLeft;
+            BottomRight = bottomRight;
+        }
+        public Rect(Point topLeft, int width, int height) : this(topLeft, (topLeft.X + width, topLeft.Y + height)) { }
+        public Rect(int x1, int y1, int x2, int y2) : this((x1, y1), (x2, y2)) { }
+
+        void MaintainStructure() {
+            if (TopLeft.X > TopRight.X) (TopLeft, TopRight) = (TopRight, TopLeft);
+            if (BottomRight.Y < TopRight.Y) (BottomRight, TopRight) = (TopRight, BottomRight);
+        }
+
+        public static bool operator ==(Rect a, Rect b) =>
+            a.TopLeft == b.TopLeft &&
+            a.TopRight == b.TopRight &&
+            a.BottomLeft == b.BottomLeft &&
+            a.BottomRight == b.BottomRight;
+        public static bool operator !=(Rect a, Rect b) => !(a == b);
+
+        public override string ToString() => $"[{TopLeft},{TopRight},{BottomLeft},{BottomRight}]";
+    }
+}
