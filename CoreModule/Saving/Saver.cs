049bcdae181208bd3ec32cddc860cb41da9b4d50
Gavin White <horace5413@gmail.com>
2020-04-07 16:39:54 -0700

    Document Saver.cs

--- CoreModule/Saving/Saver.cs
+++ CoreModule/Saving/Saver.cs
@@ -55,8 +55,10 @@ namespace CoreModule.Saving {
 
                 foreach (Attribute attribute in member.GetCustomAttributes()) {
                     if (attribute is At saveto) { // Only save member if it has a location in the save file
-                        Type t = null;
+                        #region Find member type and value
+                        Type t = null; // MemberInfo does not contain type or value directly
                         object value = null;
+
                         if (member is PropertyInfo p) {
                             t = p.PropertyType;
                             value = p.GetValue(obj);
@@ -65,34 +67,43 @@ namespace CoreModule.Saving {
                             t = f.FieldType;
                             value = f.GetValue(obj);
                         }
+                        #endregion Find member type and value
 
+                        #region Work with member
+                        #region Check if member is Saveable
                         var memberTypeAttributes = t.GetCustomAttributes();
                         bool memberIsSaveable = false;
+
                         foreach (Attribute a in memberTypeAttributes) {
                             if (a is Saveable) {
                                 memberIsSaveable = true;
                                 break;
                             }
                         }
+                        #endregion Check if member is Saveable
 
-                        if (memberIsSaveable) {
+                        if (memberIsSaveable) { // If the member itself is a Saveable type, save it
                             if (value != null)
                                 Save(value, $"{oldPath}{name}/{(saveto.Folder == "root" ? "" : saveto.Folder)}", member.Name);
                         }
-                        else {
+                        else { // Otherwise, add the member to the dictionary of members to save to files
                             if (!folders.ContainsKey(saveto.Folder)) folders[saveto.Folder] = new List<MemberInfo>();
                             folders[saveto.Folder].Add(member);
                         }
+                        #endregion Work with member
                     }
                 }
             }
             #endregion Find save structure
 
+            #region Write non-Saveable members to files
             foreach (string s in folders.Keys) {
-                string subfolderPath = $"{root}{s}/";
-                if (s == "root") subfolderPath = root;
+                string subfolderPath = $"{root}{s}/"; // Get the global path to the subfolder within the save the member resides in
+                if (s == "root") subfolderPath = root; // If the member is saved At("root"), the subfolder is the object's root folder
                 if (!Directory.Exists(subfolderPath)) Directory.CreateDirectory(subfolderPath);
+
                 foreach (MemberInfo info in folders[s]) {
+                    #region Get member type and value
                     object v = null;
                     Type t = null;
 
@@ -104,49 +115,71 @@ namespace CoreModule.Saving {
                         v = f.GetValue(obj);
                         t = f.FieldType;
                     }
+                    #endregion Get member type and value
 
-                    if (Serializers.ContainsKey(t)) {
-                        string pathToMember = $"{subfolderPath}{info.Name}";
-                        Directory.CreateDirectory(pathToMember);
+                    #region Write member to file
+                    if (Serializers.ContainsKey(t)) { // If the saver knows how to serialize the member
+                        string pathToMember = $"{subfolderPath}{info.Name}"; // Get global path to the name folder
+                        Directory.CreateDirectory(pathToMember); // Create folder for member data
 
                         try {
-                            File.WriteAllBytes(pathToMember + "/type", Encoding.ASCII.GetBytes(t.ToString()));
-                            File.WriteAllBytes(pathToMember + "/data", Serializers[t](v));
+                            File.WriteAllBytes(pathToMember + "/type", Encoding.ASCII.GetBytes(t.ToString())); // Write type of member to file
+                            File.WriteAllBytes(pathToMember + "/data", Serializers[t](v)); // Write data of member to file
                         }
                         catch { }
                     }
-                    else throw new ArgumentException($"Cannot serialize type {t}");
+                    else throw new ArgumentException($"Cannot serialize type {t}"); // Otherwise, can't serialize
+                    #endregion Write member to file
                 }
             }
+            #endregion Write non-Saveable members to file
 
-            if (File.Exists($"{final}.backup")) File.Delete($"{final}.backup");
-            if (File.Exists(final)) File.Move(final, $"{final}.backup");
-            ZipFile.CreateFromDirectory(root, final);
-            Directory.Delete(root, true);
+            #region Clean up files
+            if (File.Exists($"{final}.backup")) File.Delete($"{final}.backup"); // If a backup exists, delete it
+            if (File.Exists(final)) File.Move(final, $"{final}.backup"); // Create new backup
+            ZipFile.CreateFromDirectory(root, final); // Move working directory to a .save file
+            Directory.Delete(root, true); // Delete the working directory
+            #endregion Clean up files
             #endregion Object is saveable
         }
 
+        /// <summary>
+        /// Loads a Saveable from a .save
+        /// </summary>
+        /// <param name="path">path to directory</param>
+        /// <param name="name">name of .save</param>
         public static object Load(string path, string name) {
-            if (!path.EndsWith("/")) path += "/";
-            string oldPath = path;
-            path = $"{AppDomain.CurrentDomain.BaseDirectory}/{path}/";
-            string root = $"{path}{name}";
+            #region Path Housekeeping
+            if (!path.EndsWith("/")) path += "/"; // Make sure path ends with /
+            string oldPath = path; // store the local path
+            path = $"{AppDomain.CurrentDomain.BaseDirectory}/{path}/"; // Get the global path
+            string root = $"{path}{name}"; // Get the working directory
+            #endregion Path Housekeeping
 
-            if (Directory.Exists(root)) Directory.Delete(root, true);
-            ZipFile.ExtractToDirectory(root + ".save", root);
+            if (Directory.Exists(root)) Directory.Delete(root, true); // If a working directory exists, delete it
+            ZipFile.ExtractToDirectory(root + ".save", root); // Extract the .save to the working directory
 
-            Type type = Type.GetType(File.ReadAllText($"{root}/type"));
-            object obj = Activator.CreateInstance(type);
-            object[] attributes = type.GetCustomAttributes(true);
+            #region Get type to load
+            Type type = Type.GetType(File.ReadAllText($"{root}/type")); // Parse object type from it's type file
+            object obj = Activator.CreateInstance(type); // Create an instance of an object of that type
+            object[] attributes = type.GetCustomAttributes(true); // Get the attributes of the object
 
+            #region Check if type is Saveable
             bool loadable = false;
+
             foreach (object attribute in attributes)
                 if (attribute is Saveable) loadable = true;
-            if (!loadable) throw new ArgumentException("Object is not saveable");
+            if (!loadable) throw new ArgumentException("Object is not loadable"); // Type in .save typefile isn't Saveable, something went wrong
+            #endregion Check if type is Saveable
+            #endregion Get type to load
 
+            #region Load members
             Dictionary<string, List<MemberInfo>> folders = new Dictionary<string, List<MemberInfo>>();
+
+            #region Get members to load
             foreach (MemberInfo member in type.GetMembers()) {
                 if (member.MemberType != MemberTypes.Field && member.MemberType != MemberTypes.Property) continue;
+
                 foreach (Attribute attribute in member.GetCustomAttributes()) {
                     if (attribute is At saveto) {
                         Type t = null;
@@ -166,7 +199,7 @@ namespace CoreModule.Saving {
                             }
                         }
 
-                        if (memberIsSaveable) {
+                        if (memberIsSaveable) { // If the member type is Saveable, load it from it's own embedded .save
                             if (member is PropertyInfo propertyInfo) {
                                 if (File.Exists($"{AppDomain.CurrentDomain.BaseDirectory}/{oldPath}{name}/{(saveto.Folder == "root" ? "" : saveto.Folder)}/{member.Name}.save"))
                                     propertyInfo.SetValue(obj, Load(oldPath + name + $"/{(saveto.Folder == "root" ? "" : saveto.Folder)}", member.Name));
@@ -176,20 +209,22 @@ namespace CoreModule.Saving {
                                     fieldInfo.SetValue(obj, Load(oldPath + name + $"/{(saveto.Folder == "root" ? "" : saveto.Folder)}", member.Name));
                             }
                         }
-                        else {
+                        else { // Otherwise, add it to the list of members to load from file
                             if (!folders.ContainsKey(saveto.Folder)) folders[saveto.Folder] = new List<MemberInfo>();
                             folders[saveto.Folder].Add(member);
                         }
                     }
                 }
             }
+            #endregion Get members to load
 
+            #region Load non-Saveable members
             foreach (string s in folders.Keys) {
                 string subfolderPath = $"{root}/{s}/";
                 if (s == "root") subfolderPath = root;
+
                 foreach (MemberInfo info in folders[s]) {
                     string pathToMember = $"{subfolderPath}/{info.Name}";
-
                     Type t = Type.GetType(File.ReadAllText(pathToMember + "/type"));
 
                     if (Deserializers.ContainsKey(t)) {
@@ -206,9 +241,11 @@ namespace CoreModule.Saving {
                     else throw new ArgumentException($"Cannot deserialize type {t}");
                 }
             }
-            Directory.Delete(root, true);
+            #endregion load non-Saveable members
+            #endregion Load members
 
-            return obj;
+            Directory.Delete(root, true); // Remove working directory
+            return obj; // Return loaded object
         }
     }
 }
